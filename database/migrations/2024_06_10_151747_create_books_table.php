3772dd084278127546b09d8225f1d63ffad66b37
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-06-18 14:30:45 +0200

    ADD: Setting up DB

--- database/migrations/2024_06_10_151747_create_books_table.php
+++ database/migrations/2024_06_10_151747_create_books_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->string('author');
+
+            
             $table->timestamps();
         });
     }
