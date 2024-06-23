8f1b351266d10299cc403b9c68d1ef07260b1d82
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-06-23 18:02:52 +0200

    ADD: Popular or Highest Rated Controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,13 +13,23 @@ class BookController extends Controller
     public function index(Request $request)
 
     {
-        $title = $request->query('title');
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
             fn ($query, $title) => 
             $query->title($title)
-        )->get();
+        );
+
+        $books = match($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6month' => $books->popularLast6Month(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6month' => $books->highestRatedLast6Month(),
+            default => $books->latest()
+        };
+        $books = $books->get();
 
         //return view('books.index', compact('books'));
         return view('books.index', ['books' => $books]);
